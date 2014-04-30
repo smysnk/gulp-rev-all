a14b2335861472b286fdf7e1ee9e707e84d943c3
sloops77 <andres.olave@thinkingbytes.net>
2014-04-30 15:14:52 +0300

    Fixes test execution

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var revall = require("../");
+var revall = require("../index");
 var toolsFactory = require("../tools");
 var should = require("should");
 var gulp = require("gulp");
@@ -8,6 +8,8 @@ var util = require("util");
 var Stream = require("stream");
 var assert = require("assert");
 var path = require('path');
+var gutil = require("gulp-util");
+var glob = require("glob");
 
 require("mocha");
 
@@ -15,45 +17,64 @@ describe("gulp-rev-all", function() {
 
     var tools = toolsFactory({});
 
-    it("should revision images without corrupting them", function(done) {
+    describe('should process images', function() {
+        var stream;
 
-        var stream = gulp.src('fixtures/config1/img/image1.jpg')
-            .pipe(revall())
-            .pipe(es.map(function(file, cb) {
-                
+        beforeEach(function (done) {
+            stream = revall({rootDir:'test/fixtures/config1'})
+            done()
+        });
+
+        var writeFile = function() {
+            stream.write(new gutil.File({
+                path: 'test/fixtures/config1/img/image1.jpg',
+                contents: fs.readFileSync('test/fixtures/config1/img/image1.jpg')
+            }));
+        }
+
+        it("should revision images without corrupting them", function(done) {
+            stream.on('data', function (file) {
                 file.contents[0].should.equal(255);
                 file.contents[1].should.equal(216);
                 file.contents[2].should.equal(255);
                 file.contents[3].should.equal(224);
-                cb(null, file);
-                
-            }));
+                done();
+            });
 
-        stream.on('end', done);
-
-    });
+            writeFile();
+        });
+    })
 
     describe("options:", function() {
+
+        var stream;
+
+        var writeFile = function() {
+            //write all files to stream
+            glob("test/fixtures/config1/**/*.*", {}, function (er, fileNames) {
+                fileNames.forEach(function (fileName) {
+                    stream.write(new gutil.File({
+                        path: fileName,
+                        contents: fs.readFileSync(fileName)
+                    }));
+                });
+
+                stream.end()
+            });
+        }
 
         describe("hash length", function() {
 
             it("should have proper length when specified", function(done) {
 
-                var stream = gulp.src('fixtures/config1/**')
-                    .pipe(revall({ hashLength: 4 }))
-                    .pipe(es.map(function(file, cb) {
-                        
-                        // Check files only, ignore directories
-                        if (!file.isNull()) { 
-                            path.basename(file.path).should.match(/\-[a-z0-9]{4}\.[a-z]{2,4}$/);
-                        }
-
-                        cb(null, file);
-                        
-                    }));
+                stream = revall({rootDir: 'test/fixtures/config1', hashLength: 4});
+                stream.on('data', function (file) {
+                    path.basename(file.path).should.match(/\-[a-z0-9]{4}\.[a-z]{2,4}$/);
+                });
 
                 stream.on('end', done);
 
+                writeFile();
             });
 
         });
@@ -61,230 +82,243 @@ describe("gulp-rev-all", function() {
 
         describe("ignore extension", function() {
 
-            it("should not rename html files when specified", function(done) {
 
-                gulp.src('fixtures/config1/**')
-                    .pipe(revall({ ignoredExtensions: ['.html'] }))
-                    .pipe(es.map(function(file, cb) {
-                        
-                        path.basename(file.path).should.not.match(/\-[a-z0-9]{8}\.html$/);
-
-                        cb(null, file);
-                        
-                    }));
-
-                gulp.src('fixtures/config1/**')
-                    .pipe(revall({ ignoredExtensions: ['.js'] }))
-                    .pipe(es.map(function(file, cb) {
-                        
-                        path.basename(file.path).should.not.match(/\-[a-z0-9]{8}\.js$/);
-
-                        cb(null, file);
-                        
-                    }));
-
-                var stream = gulp.src('fixtures/config1/**')
-                    .pipe(revall({ ignoredExtensions: ['.woff'] }))
-                    .pipe(es.map(function(file, cb) {
-                        
-                        path.basename(file.path).should.not.match(/\-[a-z0-9]{8}\.woff$/);
-
-                        cb(null, file);
-                        
-                    }));
-
+            it("should not rename html files when specified", function (done) {
+                stream = revall({rootDir: 'test/fixtures/config1', ignoredExtensions: ['.html']});
+                stream.on('data', function (file) {
+                    path.basename(file.path).should.not.match(/\-[a-z0-9]{8}\.html$/);
+                });
 
                 stream.on('end', done);
 
+                writeFile();
             });
 
+
+            it("should not rename js files when specified", function (done) {
+                stream = revall({rootDir: 'test/fixtures/config1', ignoredExtensions: ['.js']});
+                stream.on('data', function (file) {
+                    path.basename(file.path).should.not.match(/\-[a-z0-9]{8}\.js$/);
+                });
+                stream.on('end', done);
+
+                writeFile();
+            });
+
+
+            it("should not rename woff files when specified", function (done) {
+                stream = revall({rootDir: 'test/fixtures/config1', ignoredExtensions: ['.woff']});
+                stream.on('data', function (file) {
+                    path.basename(file.path).should.not.match(/\-[a-z0-9]{8}\.woff$/);
+                });
+                stream.on('end', done);
+
+                writeFile();
+            });
         });
 
     });
 
     describe("root html", function() {
+        var stream;
+
+        beforeEach(function (done) {
+            stream = revall({rootDir:'test/fixtures/config1'})
+            done()
+        });
+
+        var writeFile = function() {
+            stream.write(new gutil.File({
+                path: 'test/fixtures/config1/index.html',
+                contents: fs.readFileSync('test/fixtures/config1/index.html')
+            }));
+        }
 
         it("should resolve reference to css", function(done) {
-            
-            var stream = gulp.src('fixtures/config1/index.html')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
-                    
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/css/style.css'));
-                    String(file.contents).should.containEql(revedReference);
-                    cb(null, file);
-                    
-                }));
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/css/style.css'));
+                String(file.contents).should.containEql(revedReference);
+                done();
+            });
 
-            stream.on('end', done);
-
+            writeFile();
         });
 
         it("should resolve reference reference to angularjs view", function(done) {
-            
-            var stream = gulp.src('fixtures/config1/index.html')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/view/main.html'));
+                String(file.contents).should.containEql(revedReference);
+                done();
+            });
 
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/view/main.html'));                    
-                    String(file.contents).should.containEql(revedReference);
-                    cb(null, file);
-                    
-                }));
-
-            stream.on('end', done);
-
+            writeFile();
         });
 
 
         it("should resolve reference reference to javascript include", function(done) {
-            
-            var stream = gulp.src('fixtures/config1/index.html')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/application.js'));
+                String(file.contents).should.containEql(revedReference);
+                done();
+            });
 
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/application.js'));                    
-                    String(file.contents).should.containEql(revedReference);
-                    cb(null, file);
-                    
-                }));
-
-            stream.on('end', done);
-
+            writeFile();
         });
 
 
         it("should resolve reference in double quotes ", function(done) {
-            
-            var stream = gulp.src('fixtures/config1/index.html')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
 
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/img/image1.jpg'));                    
-                    String(file.contents).should.containEql(revedReference);
-                    cb(null, file);
-                    
-                }));
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/img/image1.jpg'));
+                String(file.contents).should.containEql(revedReference);
+                done();
+            });
 
-            stream.on('end', done);
-
+            writeFile();
         });
 
         it("should resolve reference in single quotes", function(done) {
 
-            
-            var stream = gulp.src('fixtures/config1/index.html')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
-                    
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/img/image2.jpg'));
-                    String(file.contents).should.containEql(revedReference);
-                    cb(null, file);
-                    
-                }));
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/img/image2.jpg'));
+                String(file.contents).should.containEql(revedReference);
+                done();
+            });
 
-            stream.on('end', done);
+            writeFile();
+        });
 
+        it("should replace all references", function(done) {
+
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/img/image3.jpg'));
+                var count = String(file.contents).match(RegExp(revedReference, 'g'));
+                count.length.should.containEql(2);
+                done();
+            });
+
+            writeFile();
         });
 
     });
 
     describe("angularjs view", function() {
+        var stream;
 
-        it("should resolve references to images", function(done) {
-            
-            var stream = gulp.src('fixtures/config1/view/main.html')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
-                    
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/img/image1.jpg'));
-                    String(file.contents).should.containEql(revedReference);
-                    cb(null, file);
-                    
-                }));
-
-            stream.on('end', done);
-
+        beforeEach(function (done) {
+            stream = revall({rootDir:'test/fixtures/config1'})
+            done()
         });
 
-        it("should resolve references to angular incldues", function(done) {
+        var writeFile = function() {
+            stream.write(new gutil.File({
+                path: 'test/fixtures/config1/view/main.html',
+                contents: fs.readFileSync('test/fixtures/config1/view/main.html')
+            }));
+        }
 
-            var stream = gulp.src('fixtures/config1/view/main.html')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
-                    
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/view/core/footer.html'));
-                    String(file.contents).should.containEql(revedReference);
+        it("should resolve references to images", function(done) {
 
-                    cb(null, file);                    
-                }));
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/img/image1.jpg'));
+                String(file.contents).should.containEql(revedReference);
+                done();
+            });
 
-            stream.on('end', done);
+            writeFile();
+        });
 
+        it("should resolve references to angular includes", function(done) {
+
+            stream.on('data', function (file) {
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/view/core/footer.html'));
+                String(file.contents).should.containEql(revedReference);
+                done();
+            });
+
+            writeFile();
         });
 
     });
 
-    describe("css", function() {
+    describe("css", function() { var stream;
 
-        it("should resolve references to fonts", function(done) {
+        beforeEach(function (done) {
+            stream = revall({rootDir:'test/fixtures/config1'})
+            done()
+        });
 
-            var stream = gulp.src('fixtures/config1/css/style.css')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
-                    
-                    var revedReference1 = path.basename(tools.revFile('fixtures/config1/font/font1.eot'));
-                    String(file.contents).should.containEql(revedReference1);
+        var writeFile = function() {
+            stream.write(new gutil.File({
+                path: 'test/fixtures/config1/css/style.css',
+                contents: fs.readFileSync('test/fixtures/config1/css/style.css')
+            }));
+        }
 
-                    var revedReference2 = path.basename(tools.revFile('fixtures/config1/font/font1.woff'));
-                    String(file.contents).should.containEql(revedReference2);
+        it("should resolve references to fonts", function (done) {
+            stream.on('data', function (file) {
+                var revedReference1 = path.basename(tools.revFile('test/fixtures/config1/font/font1.eot'));
+                String(file.contents).should.containEql(revedReference1);
 
-                    var revedReference3 = path.basename(tools.revFile('fixtures/config1/font/font1.ttf'));
-                    String(file.contents).should.containEql(revedReference3);
+                var revedReference2 = path.basename(tools.revFile('test/fixtures/config1/font/font1.woff'));
+                String(file.contents).should.containEql(revedReference2);
 
-                    var revedReference4 = path.basename(tools.revFile('fixtures/config1/font/font1.svg'));
-                    String(file.contents).should.containEql(revedReference4);
+                var revedReference3 = path.basename(tools.revFile('test/fixtures/config1/font/font1.ttf'));
+                String(file.contents).should.containEql(revedReference3);
 
-                    cb(null, file);                    
-                }));
+                var revedReference4 = path.basename(tools.revFile('test/fixtures/config1/font/font1.svg'));
+                String(file.contents).should.containEql(revedReference4);
 
-            stream.on('end', done);
+                done();
+            });
+
+            writeFile();
 
         });
 
-        it("should resolve references to images", function(done) {
+        it("should resolve references to images", function (done) {
 
-            var stream = gulp.src('fixtures/config1/css/style.css')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
-                    
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/img/image1.jpg'));
-                    String(file.contents).should.containEql(revedReference);
+            stream.on('data', function (file) {
 
-                    cb(null, file);                    
-                }));
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/img/image1.jpg'));
+                String(file.contents).should.containEql(revedReference);
 
-            stream.on('end', done);
+                done();
+            });
 
+            writeFile();
         });
+
 
     });
 
     describe("main js", function() {
 
+        var stream;
+
+        beforeEach(function (done) {
+            stream = revall({rootDir:'test/fixtures/config1'})
+            done()
+        });
+
+        var writeFile = function() {
+            stream.write(new gutil.File({
+                path: 'test/fixtures/config1/application.js',
+                contents: fs.readFileSync('test/fixtures/config1/application.js')
+            }));
+        }
+
         it("should resolve references to angularjs views", function(done) {
 
-            var stream = gulp.src('fixtures/config1/application.js')
-                .pipe(revall({rootDir:'fixtures/config1'}))
-                .pipe(es.map(function(file, cb) {
-                    
-                    var revedReference = path.basename(tools.revFile('fixtures/config1/view/gps.html'));
-                    String(file.contents).should.containEql(revedReference);
+            stream.on('data', function (file) {
 
-                    cb(null, file);                    
-                }));
+                var revedReference = path.basename(tools.revFile('test/fixtures/config1/view/gps.html'));
+                String(file.contents).should.containEql(revedReference);
 
-            stream.on('end', done);
+                done()
+            });
+
+            writeFile();
 
         });
 
