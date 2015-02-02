263e5b1464e3af7866b1e4371477b4d6006d27d3
Petter <petter@petterhaggholm.net>
2015-02-02 10:38:56 -0800

    For relative paths, prefer dir-relative refs

--- tool.js
+++ tool.js
@@ -272,12 +272,6 @@ module.exports = function(options) {
                     });
                 }
 
-                referencePaths.push({
-                    base: fileBasePath,
-                    path: joinPath(fileBasePath, reference_),
-                    isRelative: false
-                });
-
                 if (pathType === 'relative') {
                     referencePaths.push({
                         base: fileBasePath,
@@ -285,6 +279,12 @@ module.exports = function(options) {
                         isRelative: true
                     });
                 }
+
+                referencePaths.push({
+                    base: fileBasePath,
+                    path: joinPath(fileBasePath, reference_),
+                    isRelative: false
+                });
 
             }
 
